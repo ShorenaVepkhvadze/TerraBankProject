a737380c34259eabfcd9d1a991610b3cff157a03
Archil Sikharulidzee <softdevelopment@yahoo.com>
2023-02-14 21:10:44 +0400

    Add project files.

---
+++ TerraBankProject/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+* This is a learning project in scopes of the C# course fop
+* Terra Bank employes...
+* 
+*/
+
+class MyMain // name of the class, Zoo, TbilisiZooSaburtalo
+{
+    static void Main(string[] args) // entry point, from which program starts
+    {
+        // აქ გამომყავს კონსოლში შეტყობინება, single-line comment
+        Console.WriteLine("Welcome to C sharp..."); // text + გადასვლა ახალ სტრიქონზე
+        Console.WriteLine("Mr. Archil");
+    }
+
+    /// <summary>
+    /// This method counts amount of cows, pigs in the farm.
+    /// </summary>
+    static void regularMethod()
+    {
+        Console.WriteLine();
+    }
+}
