53dd5a252682a19aff908a9cfa71eb9ef0305aef
Archil Sikharulidzee <softdevelopment@yahoo.com>
2023-02-17 19:32:45 +0400

    Update

--- TerraBankProject/Program.cs
+++ TerraBankProject/Program.cs
@@ -1,28 +1,9 @@
-﻿/*
-* ეს არის პროგრამა, რომლის ფარგლებშიც ვსწავლობთ C# საფუძვლებს.
-* დამატებითი ინფორმაცია...
-*/
-class Program
+﻿class Program
 {
     static void Main(string[] args)
     {
-        // აქედან იწყება ჩემი პროგრამა
-        //getMyAge();
-        Console.WriteLine("This is the first line...");
-        Console.WriteLine(); // second
-        Console.WriteLine("Third");
-        Console.WriteLine("Fourth");
-
-        //Console.WriteLine();
+        // კონსოლში ახალი სტრიქონის გარეშე ინფორმაციის შეყვანა
         Console.Write("Archil");
         Console.Write(" Sikharulidze");
-    }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    static void getMyAge()
-    {
-        // empty method
     }
 }
