6e5996c9b94d51723667de8cbcbb05e1b42c7927
Archil Sikharulidzee <softdevelopment@yahoo.com>
2023-02-28 21:31:02 +0400

    message

--- TerraBankProject/Program.cs
+++ TerraBankProject/Program.cs
@@ -1,4 +1,4 @@
-﻿class LessonFour
+﻿class HomeworkFive
 {
     static void Main(string[] args) // შესვლის წერტილის მეთოდი, მხოლოდ ერთი
     {
@@ -11,21 +11,71 @@
         welcomingMessage();
 
         int randomNumber = getRandomNumber();
+        int usersNumber;
+        // int counter = 1;
 
-        int usersNumber = enterNumber();
+        // უნდა შევადაროთ ორი რიცხვი
+        // თუ გამოვიცანი, მაშინ მოგება
+        // თუ ვერ გამოვიცანი და ჩემი რიცხვი ჩაფიქრებულზე ნაკლებია - შესაბამისი შეტყობინება
+        // თუ ვერ გამოვიცანი და ჩემი რიცხვი ჩაფიქრებულზე მეტია - შესაბამისი შეტყობინება
 
-        if (randomNumber == usersNumber)
+        // ფუნქციონალის დამატება - უნდა მქონდეს სამი ცდა
+        // მესამე ცდის ბოლოს, თუ ვერ გამოვიცანი, მაშინ წავაგე
+        // თუ გამოვიცანი - მოვიგე
+
+        //while(counter <= 3)
+        //{
+        //usersNumber = enterNumber();
+
+        //if (randomNumber == usersNumber)
+        // {
+        //    Console.WriteLine("You have WON!");
+        //     return; // ყველაფერი დასრულდება გაგრძელების გარეშე
+        //}
+        // else if (randomNumber > usersNumber && counter != 3)
+        // {
+        //     Console.WriteLine("Your number is lower than random number.");
+        // }
+        // else if (randomNumber < usersNumber && counter != 3)
+        // {
+        // Console.WriteLine("Your number is higher than random number.");
+        // }
+        // counter++;
+        // }
+
+        //for (int counter = 1; counter <= 3; counter++)
+        // {
+        // usersNumber = enterNumber();
+
+        // if (randomNumber == usersNumber)
+        //{
+        // Console.WriteLine("You have WON!");
+        //  return; // ყველაფერი დასრულდება გაგრძელების გარეშე
+        // }
+        // else if (randomNumber > usersNumber && counter != 3)
+        // {
+        //    Console.WriteLine("Your number is lower than random number.");
+        //}
+        // else if (randomNumber < usersNumber && counter != 3)
+        // {
+        // Console.WriteLine("Your number is higher than random number.");
+        // }
+        //  }
+        //Console.WriteLine("You have LOST!");
+
+        for (int counter = 1; counter <= 3; counter++)
         {
-            Console.WriteLine("You have WON!");
+            usersNumber = enterNumber();
+            if (checkGame(usersNumber, randomNumber, counter))
+            {
+                return;
+            }
+            Console.WriteLine("You have LOST!");
         }
-        else if (randomNumber > usersNumber)
-        {
-            Console.WriteLine("Your number is lower than random number.");
-        }
-        else if (randomNumber < usersNumber)
-        {
-            Console.WriteLine("Your number is higher than random number.");
-        }
+        Console.WriteLine("You have LOST!");
+
+        // კიდევ ერთი ციკლი შევქმნა, რომელიც მკითხავს მინდა თუ არა, რომ ახლიდან დავიწყო თამაში
+        // თუ კი, მაშინ უნდა ყველაფერი განახლდეს და ახლიდან დაიწყოს. თუ არა, მაშინ ყველაფერი დასრულდეს.
     }
 
     static void welcomingMessage()
@@ -49,5 +99,23 @@
         Console.Write("Please, enter a number: ");
 
         return Int32.Parse(Console.ReadLine());
+    }
+
+    static bool checkGame(int usersNumber, int randomNumber, int counter)
+    {
+        if (randomNumber == usersNumber)
+        {
+         Console.WriteLine("You have WON!");
+          return true; // ყველაფერი დასრულდება გაგრძელების გარეშე
+         }
+         else if (randomNumber > usersNumber && counter != 3)
+         {
+            Console.WriteLine("Your number is lower than random number.");
+        }
+         else if (randomNumber < usersNumber && counter != 3)
+         {
+         Console.WriteLine("Your number is higher than random number.");
+         }
+        return false;
     }
 }
