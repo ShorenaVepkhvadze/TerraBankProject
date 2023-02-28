7495df6f922a1e9e566e2bb31e0977284ce354a1
Archil Sikharulidzee <softdevelopment@yahoo.com>
2023-02-28 19:11:50 +0400

    Updates

--- TerraBankProject/Program.cs
+++ TerraBankProject/Program.cs
@@ -2,80 +2,52 @@
 {
     static void Main(string[] args) // შესვლის წერტილის მეთოდი, მხოლოდ ერთი
     {
-        Console.WriteLine(getIntegerSum(91, 78)); // method call
-        // class არსებილი სახელი, პირველი ასო უნდა იყოს დიდი
-        // ცვლადები უნდა იყოს არსებითი სახელი და აგრეთვე იწერებოდეს myAge, age
-        // მეთოდი არის ზმნა calculateBMI, getMyAge
-        // მეთოდი არის ზმნა CalculateBMI, GetMyAge
+        // Game
+        startGame();
     }
 
-    static void homework()
+    static void startGame()
     {
-        /* 1.შევქმნათ სამი ცვლადი(x, z, y). მივანიჭოთ მნიშვნელობები. 
- * გამოვიყვანოთ კონსოლში: x + z * y = ჯამი; მაგ. 4, 5, 10 - ის შედეგია 54.
- */
-        int x = 4;
-        int z = 2;
-        int y = 5;
+        welcomingMessage();
 
-        int sum = x + (z * y); // ()
-        Console.WriteLine("The sum of x (4) + z (2) * y (5) equals to: " + sum);
+        int randomNumber = getRandomNumber();
 
-        Console.WriteLine();
-        getMaxMin();
+        int usersNumber = enterNumber();
+
+        if (randomNumber == usersNumber)
+        {
+            Console.WriteLine("You have WON!");
+        }
+        else if (randomNumber > usersNumber)
+        {
+            Console.WriteLine("Your number is lower than random number.");
+        }
+        else if (randomNumber < usersNumber)
+        {
+            Console.WriteLine("Your number is higher than random number.");
+        }
     }
 
-    /// <summary>
-    /// 1. შევქმნათ მეთოდი, რომელსაც გადავცემთ ორ სრულ რიცხვს.მეთოდმა უნდა 
-    /// დააბრუნოს ორი რიცხვის ჯამი.გამოვიყვანოთ ეს ჯამი შესვლის წერტილის მეთოდის 
-    /// მეშვეობით კონსოლში.
-    /// </summary>
-    static int getIntegerSum(int varOne, int varTwo)
+    static void welcomingMessage()
     {
-        return varOne + varTwo;
+        Console.WriteLine("Let's play a game. I have a hidden number for you.");
+        Console.WriteLine("This number is between 0 to 20. Please, guess it.");
+        Console.WriteLine("You have three attempts.");
     }
 
-    /// <summary>
-    /// 2. შევქმნათ მეთოდი. ვთხოვოთ მომხმარებელს შეიყვანოს ოთხი ნებისმიერი ტიპის 
-    /// რიცხვი (ანუ არა მხოლოდ ინტეჯერი). მეთოდის ფარგლებში უნდა მოიძებნოს ამ 
-    /// ოთხი რიცხვიდან ყველაზე დიდი და ყველაზე პატარა. მეთოდმა უნდა გამოიყვანოს 
-    /// კონსოლში ეს ორი რიცხვი.
-    /// </summary>
-    static void getMaxMin()
+    static int getRandomNumber()
     {
-        Console.Write("Please, enter the first number: ");
-        double numbOne = Double.Parse(Console.ReadLine());
-        Console.Write("Please, enter the second number: ");
-        double numbTwo = Double.Parse(Console.ReadLine());
-        Console.Write("Please, enter the third number: ");
-        double numbThree = Double.Parse(Console.ReadLine());
-        Console.Write("Please, enter the fourth number: ");
-        double numbFour = Double.Parse(Console.ReadLine());
+        //Random random = new Random();
+        //int randomNumber = random.Next(0, 21);
+        //return randomNumber;
 
-        Console.WriteLine(numbOne + " " + numbTwo + " " + numbThree + " " + numbFour);
+        return new Random().Next(0, 21);
+    }
 
-        double max = 0; // temporary
-        double min = 0;
+    static int enterNumber()
+    {
+        Console.Write("Please, enter a number: ");
 
-        if (numbOne > numbTwo)
-        {
-            max = numbOne;
-        }
-        else
-        {
-            max = numbTwo;
-        }
-
-        if (max < numbThree)
-        {
-            max = numbThree;
-        }
-
-        if (max < numbFour)
-        { 
-            max = numbFour;
-        }
-
-        Console.WriteLine("The MAX number is: " + max);
+        return Int32.Parse(Console.ReadLine());
     }
 }
